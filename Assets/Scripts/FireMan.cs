de8cbd5de128e651e19dbac398da3a9a58fb4775
Ibraheem Asad <ibo.asad.97@gmail.com>
2022-12-18 17:17:29 +0200

    Movement editing
    added singleton and changed fireman movement

--- Assets/Scripts/FireMan.cs
+++ Assets/Scripts/FireMan.cs
@@ -1,64 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FireMan : MonoBehaviour
 {
-    [SerializeField] private int speed;
-
     private Transform _t;
-    private Vector3 _moveDirection;
     private Rigidbody2D _rb;
-    private float _rightAngle = 90;
-    private float _leftAngle = -90;
-    private float _upAngle = 180;
-    private float _downAngle = 0;
-    private float _chosenAngle;
-    private Vector3 _currentAngle;
+
+    // movement
+    [SerializeField] private int movingSpeed;
+    private Vector2 _moveDirection;
+    
     
     private KeyCode _right = KeyCode.D,
-        _left = KeyCode.A,
-        _up = KeyCode.W,
-        _down = KeyCode.S;
+                    _left = KeyCode.A,
+                    _up = KeyCode.W,
+                    _down = KeyCode.S;
     
     // Start is called before the first frame update
     void Start()
     {
         _t = GetComponent<Transform>();
         _rb = GetComponent<Rigidbody2D>();
-        _moveDirection = Vector3.zero;
-        _chosenAngle = _t.rotation.z;
+        _moveDirection = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
-        // Lerp right and left
-
-        if (Input.GetKey(_right))
-        {
-            _moveDirection = Vector3.right;
-            _chosenAngle = _rightAngle;
-        }
-        if (Input.GetKey(_left))
-        {
-            _moveDirection = Vector3.left;
-            _chosenAngle = _leftAngle;
-        }
-        if (Input.GetKey(_up))
-        {
-            _moveDirection = Vector3.up;
-            _chosenAngle = _upAngle;
-        }
-        if (Input.GetKey(_down))
-        {
-            _moveDirection = Vector3.down;
-            _chosenAngle = _downAngle;
-        }
-
-        // _currentAngle = new Vector3(0, 0, Mathf.LerpAngle(_currentAngle.z, _chosenAngle, Time.fixedDeltaTime));
-        // _rb.MovePosition((_t.position + _moveDirection * (speed * Time.fixedDeltaTime)).normalized);
-        _moveDirection = Vector3.zero;
-        _chosenAngle = _t.rotation.z;
+        var xDirection = Input.GetAxis("Horizontal2");
+        var yDirection = Input.GetAxis("Vertical2");
+        
+        // if 4 Directions
+        // _moveDirection = GameManager.UpdateMoveDirection(_moveDirection, xDirection, yDirection);
+        
+        // else
+        _moveDirection.x = xDirection;
+        _moveDirection.y = yDirection;
+        _moveDirection.Normalize();
+        // float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+    }
+    
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _moveDirection * (movingSpeed * Time.fixedDeltaTime));
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
+    // this is a declaration for the singleton, maybe it's not needed, keep it for now. (if made problem DELETE)
+    protected GameManager(){}
+    
+    // Constants:
+    public const float RightAngle = 90;
+    public const float LeftAngle = -90;
+    public const float UpAngle = 180;
+    public const float DownAngle = 0;
+    
+    
+    // 
+    public static Vector2 UpdateMoveDirection(Vector2 oldDirection, float xDirection, float yDirection)
+    {
+        // change the direction of the movement depends on the input direction and the old direction
+        // the direction will be -1,0,1 for x and y
+
+        var newDir = Vector2.zero;
+        // if moveDirection != zero, then we were moving to one of the directions:
+        if (oldDirection != Vector2.zero)
+        {
+            // we are moving in the X axis
+            if (oldDirection.x != 0)
+            {
+                newDir.x = xDirection;
+                // we continue moving in the x axis, else if we stopped moving in x axis, move toward y axis
+                newDir.y = xDirection != 0 ? 0 : yDirection;
+            }
+            // we are moving in the Y axis
+
+            else
+            {
+                newDir.y = yDirection;
+                // we continue moving in the y axis, else if we stopped moving in y axis, move toward x axis
+                newDir.x = yDirection != 0 ? 0 : xDirection;
+            }
+        }
+        // if moveDirection is zero, we need to choose a direction (i guess we should go with X first)
+        else
+        {
+            newDir.x = xDirection;
+            // we move in the x axis, else move toward y axis
+            newDir.y = xDirection != 0 ? 0 : yDirection;
+        }
+
+        return newDir;
+    }
+
+    
+    
+    
+    
+    
     void Start()
     {
         
@@ -15,4 +66,6 @@ public class GameManager : MonoBehaviour
     {
         
     }
+    
+    
 }
