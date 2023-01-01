e2d5021847fd1e56f4f57bf78c802493c668e333
Ibraheem Asad <ibo.asad.97@gmail.com>
2023-01-01 21:10:07 +0200

    fixed changing direction spray water

--- Assets/Scripts/WaterGun.cs
+++ Assets/Scripts/WaterGun.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WaterGun : MonoBehaviour
 {
     private WaterBullet _currentWaterBullet;
+    private Vector3 _previousLookAtDirection;
 
     public void CreateWaterStream()
     {
@@ -13,7 +14,17 @@ public class WaterGun : MonoBehaviour
 
     public void EnlargeWaterStream(Vector3 playerCurrentPos, Vector3 playerLookAtDirection, Vector3 startPosition)
     {
-        _currentWaterBullet.EnlargeBullet(playerCurrentPos,  playerLookAtDirection, startPosition);
+        if (_previousLookAtDirection.Equals(playerLookAtDirection))     // if we didn't change our direction, then enlarge
+        {
+            _currentWaterBullet.EnlargeBullet(playerCurrentPos,  playerLookAtDirection, startPosition);
+        }
+        else        // we changed our direction, shoot what you used to have, and create a new one, and enlarge it
+        {
+            _currentWaterBullet.ShootBullet();
+            _currentWaterBullet = GameManager.instance.WaterBulletPool.Get();
+            _previousLookAtDirection = playerLookAtDirection; 
+            _currentWaterBullet.EnlargeBullet(playerCurrentPos,  playerLookAtDirection, startPosition);
+        }
     }
     
     public void ShootWaterStream()
