57a014308e169cda21d64ab83e4a68d988feb4d1
Ibraheem Asad <ibo.asad.97@gmail.com>
2023-01-11 16:20:10 +0200

    finished flammable class

--- Assets/Scripts/WaterBullet.cs
+++ Assets/Scripts/WaterBullet.cs
@@ -235,6 +235,10 @@ public class WaterBullet : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D col)
     {
+        if (col.CompareTag("Tree"))
+        {
+            print("");
+        }
         if (col.gameObject.TryGetComponent(out Flammable res))
         {
             res.SetSelfWatering(true);
@@ -260,6 +264,16 @@ public class WaterBullet : MonoBehaviour
     
     private void OnTriggerStay2D(Collider2D col)
     {
+        
+        if (col.CompareTag("Tree"))
+        {
+            print("");
+        }
+        if (col.gameObject.TryGetComponent(out Flammable res))
+        {
+            res.SetSelfWatering(true);
+        }
+        
         // print("there is trigger");
         if (col.CompareTag("House"))
         {
@@ -276,17 +290,22 @@ public class WaterBullet : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D col)
     {
-        if (other.gameObject.TryGetComponent(out Flammable res))
+        if (col.CompareTag("Tree"))
+        {
+            print("");
+        }
+        
+        if (col.gameObject.TryGetComponent(out Flammable res))
         {
             res.SetSelfWatering(false);
         }
         
-        if (other.CompareTag("House"))
+        if (col.CompareTag("House"))
         {
             // print("*HOUSE* stops watering");
-            other.GetComponent<HouseManager>().SetStatus(GameManager.HouseStatus.Normal);
+            col.GetComponent<HouseManager>().SetStatus(GameManager.HouseStatus.Normal);
             
         }
     }
